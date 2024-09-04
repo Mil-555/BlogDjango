fb21715455902e800b5f0246ed176b60bd45ce95
student20 <milusinski.ru@gmail.com>
2024-09-04 13:20:26 +0200

    FKod z 04.09

---
+++ mysite/blog/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
