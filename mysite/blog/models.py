4074563f688baffabe292558f3403187ef3df0d1
student20 <milusinski.ru@gmail.com>
2024-09-11 12:49:39 +0200

    Kod z 11.09

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,29 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
 class Post(models.Model):
+
+    STATUS_CHOICES = (('draft', 'Wersja robocza'),
+                      ('published', 'Opublikowany'),)
+
     title = models.CharField(max_length=250)
     body = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts')
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-publish',)
