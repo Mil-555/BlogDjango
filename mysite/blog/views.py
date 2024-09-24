145b1ee4774b7fc4a0c44f1d601e0a4f7e5553a4
student20 <milusinski.ru@gmail.com>
2024-09-24 10:30:37 +0200

    Kod z 24.09

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
@@ -8,3 +8,10 @@ def post_list(request):
     posts = Post.objects.filter(status='published')
     return render(request, 'blog/post/list.html',
                   {'posts': posts})
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
