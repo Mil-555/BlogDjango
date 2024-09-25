3924abe312fef6e76a571de3abc98da2e865e99c
student20 <milusinski.ru@gmail.com>
2024-09-25 13:19:56 +0200

    Kod z 25.09

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, Comment
+
 
 # Register your models here.
 #admin.site.register(Post)
@@ -11,3 +12,9 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', 'publish', 'author')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ('status', '-publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
 
@@ -9,10 +10,13 @@ def post_list(request):
     posts = Post.objects.filter(status='published')
     paginator = Paginator(posts, 3)
     page = request.GET.get('page')
-
-
-    return render(request, 'blog/post/list.html',
-                  {'posts': posts})
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',{'posts': posts, 'page': page})
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug,
@@ -22,3 +26,9 @@ def post_detail(request, year, month, day, slug):
                              publish__day=day)
     return render(request, 'blog/post/detail.html',
                   {'post': post,})
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status='published')
+    template_name = 'blog/post/list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
