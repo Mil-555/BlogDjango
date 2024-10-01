66078d2f895f17518d0b847453224af4b145f344
student20 <milusinski.ru@gmail.com>
2024-10-01 15:41:06 +0200

    Kod z 01.10

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -22,6 +23,8 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
+from django.template.defaulttags import comment
 from django.views.generic import ListView
 
 from blog.forms import CommentForm
@@ -20,24 +21,31 @@ def post_list(request):
     return render(request, 'blog/post/list.html',{'posts': posts, 'page': page})
 
 def post_detail(request, year, month, day, slug):
+
+    new_comment = None
+
     post = get_object_or_404(Post, slug=slug,
                              status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
 
+    comments = post.comments.filter(active=True)
+
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
+            new_comment.save()
 
-    else:
-        comment_form = CommentForm()
+    comment_form = CommentForm()
 
     return render(request, 'blog/post/detail.html',
                   {'post': post,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'comments':comments,
+                   'new_comment': new_comment})
 
 class PostListView(ListView):
     queryset = Post.objects.filter(status='published')
